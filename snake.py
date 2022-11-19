d386964b6a406e46301e6083994e8c2cd58ddeb5
j-lagr <joshualagrata@gmail.com>
2022-11-19 22:38:49 +0800

    added the game canvas

--- snake.py
+++ snake.py
@@ -4,10 +4,10 @@
 from tkinter import *
 import random 
 
-WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 1000
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
 SPEED = 75
-SIZE = 50
+SIZE = 25
 BODY = 3
 SNAKE_COLOR = "#964B00"
 FOOD_COLOR= "#FA8128"
@@ -18,3 +18,16 @@ window = Tk()
 window.title("Snake Pasensya")
 window.resizable(False, False)
 
+score = 0
+directions = "down"
+
+label = Label (window, text = "Highest Score:{}".format(score), font=("Comic Sans MS", 15) )
+label.pack()
+label = Label (window, text = "Score:{}".format(score), font=("Comic Sans MS", 25) )
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND, height=WINDOW_HEIGHT, width=WINDOW_WIDTH)
+canvas.pack()
+
+
+window.mainloop()
