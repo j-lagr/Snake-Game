3def78b2cbffcfe0d92f59b8474002bb756aeb50
j-lagr <joshualagrata@gmail.com>
2022-11-19 22:43:07 +0800

    center window when program is open

--- snake.py
+++ snake.py
@@ -29,5 +29,16 @@ label.pack()
 canvas = Canvas(window, bg=BACKGROUND, height=WINDOW_HEIGHT, width=WINDOW_WIDTH)
 canvas.pack()
 
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height =window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 window.mainloop()
